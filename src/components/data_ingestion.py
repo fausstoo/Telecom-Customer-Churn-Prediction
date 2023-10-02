410d7345d5bcb8f7e5dde77aa26fdc8dc2802e90
FaustoPF <faustopuchetafortin@gmail.com>
2023-10-02 18:34:01 -0300

    data_ingestion.py finished

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,52 @@
+import os
+import sys
+sys.path.append('../../data')
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path: str = "./data/raw/customer_churn_dataset.pkl"
+    train_data_path: str = "./data/raw/customer_churn_dataset-train.pkl"
+    test_data_path: str = "./data/raw/customer_churn_dataset-test.pkl"
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info("Data ingestion process initiated")
+        
+        try:
+            df = pd.read_pickle("./data/external/customer_churn_dataset.pkl")
+            logging.info("Dataset readed as Data Frame")
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            
+            df.to_pickle(self.ingestion_config.raw_data_path)
+    
+            logging.info("Data splitting initiated")
+            train_set, test_set =  train_test_split(df, test_size=0.3, random_state=42)
+          
+            train_set.to_pickle(self.ingestion_config.train_data_path)
+            test_set.to_pickle(self.ingestion_config.test_data_path)
+            
+            logging.info("Data ingestion completed")
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+            
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+        
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
